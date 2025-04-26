cd439fe02808c74ebbb94ab8890e91199c2a5b51
Rodrigo Santana <rodrigosantana.dev@gmail.com>
2025-04-26 14:48:00 -0300

    Modificando arquivos

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
