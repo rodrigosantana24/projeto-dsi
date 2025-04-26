21e33114c7dae7041f1aba0ee8d23b4a841c43cd
Rodrigo Santana <rodrigosantana.dev@gmail.com>
2025-04-26 14:48:00 -0300

    Modificando arquivos

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import { Logo } from '../components/Logo';
 import { TextInput } from '../components/TextInput';
-import { Button } from '../components/Button';
 
-export default function LoginScreen() {
+export default function LoginScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Logo />
       <Text style={styles.title}>NOME DO APP</Text>
       <TextInput placeholder="E-mail" secure={false} iconName="mail-outline" />
       <TextInput placeholder="Senha" secure={true} iconName="eye-outline" />
-      <Button title="Entrar" />
+      <Button title="Entrar" onPress={()=> navigation.navigate('Home')}/>
       <Text style={styles.footerText}>Não tem conta? cadastre-se</Text>
     </View>
   );
