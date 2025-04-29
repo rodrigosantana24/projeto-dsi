5ba9364dfffed5794b24496bb6cb6486f2e9857c
Rodrigo Santana <rodrigosantana.dev@gmail.com>
2025-04-29 18:39:48 -0300

    Adicionando tela home

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
-<<<<<<< HEAD
-import { View, Text, StyleSheet } from 'react-native';
-=======
+import { View, StyleSheet } from 'react-native';
 import { View } from 'react-native';
->>>>>>> 21e33114c7dae7041f1aba0ee8d23b4a841c43cd
 
 export default function HomeScreen( { navigation} ) {
     React.useLayoutEffect(() => {
         navigation.setOptions({
           headerBackVisible: false});
       }, [navigation]);
-<<<<<<< HEAD
-    
+      
       return (
         <View style={styles.container}></View>
   );
@@ -23,9 +19,3 @@ const styles = StyleSheet.create({
     backgroundColor: '#2F2E2E', // Cor de fundo para a tela home
   },
 });
-=======
-    return (
-        <View></View>
-  );
-}
->>>>>>> 21e33114c7dae7041f1aba0ee8d23b4a841c43cd
