c7989778af204a013d1e46aec8f8a13a1496012a
Rodrigo Santana <rodrigosantana.dev@gmail.com>
2025-04-29 19:54:57 -0300

    Adicionando tela home

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
 import AppNavigation from './navigation/AppNavigation';
 
 export default function App() {
   return <AppNavigation></AppNavigation>;
-=======
-import { SafeAreaView, StatusBar } from 'react-native';
-import LoginScreen from './screens/LoginScreen';
-import HomeScreen from './screens/HomeScreen';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
->>>>>>> 21e33114c7dae7041f1aba0ee8d23b4a841c43cd
 }
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { View } from 'react-native';
 
 export default function HomeScreen( { navigation} ) {
     React.useLayoutEffect(() => {
