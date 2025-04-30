f0f1a4adaabc5871794bb3cff734fd651fab529a
Luanv02 <limaluan32104@gmail.com>
2025-04-29 22:19:06 -0300

    Criacao movieCard e BottomTab

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, FlatList} from 'react-native';
+import MovieCard from '../components/MovieCard';
+import BottomTab from '../components/BottomTab';
+
+const movieListTest = [
+  {
+    title: 'Velozes e Furiosos 10',
+    image: require('../assets/velozes_furiosos.jpg'),
+    rating: 4.7,
+  },
+  {
+    title: 'Velozes e Furiosos 10',
+    image: require('../assets/velozes_furiosos.jpg'),
+    rating: 4.7,
+  },
+  {
+    title: 'Velozes e Furiosos 10',
+    image: require('../assets/velozes_furiosos.jpg'),
+    rating: 4.7,
+  },
+];
 
 export default function HomeScreen( { navigation} ) {
     React.useLayoutEffect(() => {
@@ -8,13 +28,29 @@ export default function HomeScreen( { navigation} ) {
       }, [navigation]);
       
       return (
-        <View style={styles.container}></View>
+        <View style={styles.container}>
+          <FlatList
+            data={movieListTest}
+            horizontal
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => (
+              <MovieCard title={item.title} image={item.image} rating={item.rating} />
+            )}
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{ paddingHorizontal: 20, paddingTop: 40 }}
+          />
+          <BottomTab />
+        </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2F2E2E', // Cor de fundo para a tela home
+    backgroundColor: '#192936'
   },
+  content: {
+    flex: 1,
+    padding: 20,
+  }
 });
