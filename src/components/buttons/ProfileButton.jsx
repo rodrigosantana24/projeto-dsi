e29164525a2e1dd6569b27cd9f3adfdf7a6f4b84
ThalysonKaraujo <thalyson.kauan7@gmail.com>
2025-05-26 20:22:03 -0300

    Finalizando ProfileButton

--- src/components/buttons/ProfileButton.jsx
+++ src/components/buttons/ProfileButton.jsx
@@ -1,3 +1,40 @@
 import React from 'react';
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
+
+const ProfileButton = ({ icon, text, onPress, showArrow = true }) => {
+  return (
+    <TouchableOpacity style={styles.menuItem} onPress={onPress}>
+      {icon}
+      <Text style={styles.menuText}>{text}</Text>
+      {showArrow && (
+        <Feather
+          name="chevron-right"
+          size={20}
+          color="white"
+          style={{ marginLeft: 'auto' }}
+        />
+      )}
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  menuItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 15,
+    borderBottomColor: '#2f3e4f',
+    borderBottomWidth: 1,
+    backgroundColor: '#16222f',
+    borderRadius: 8,
+    marginBottom: 10,
+  },
+  menuText: {
+    marginLeft: 10,
+    color: 'white',
+    fontSize: 16,
+  },
+});
+
+export default ProfileButton;
