51100814915811d43f5fbf0019770fde84086a1a
ThalysonKaraujo <thalyson.kauan7@gmail.com>
2025-05-26 20:14:57 -0300

    Criando ProfileButton e MenuScreen

---
+++ src/screens/MenuScreen.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { Ionicons, Feather, MaterialIcons, FontAwesome } from '@expo/vector-icons';
