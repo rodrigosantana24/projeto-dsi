fdd30298d7e564a90e451fd17a9d2aa078b559a3
victorsouza00 <victorsouza183@gmail.com>
2025-06-07 22:24:30 -0300

    Integração do Login com o FireBaseDB

---
+++ src/configs/firebaseConfig.jsx
@@ -0,0 +1,22 @@
+import {initializeApp} from "firebase/app"
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app)
+const database = getDatabase(app)
+
+export {auth,database}
