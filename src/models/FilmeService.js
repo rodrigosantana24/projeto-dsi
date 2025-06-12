9f04aefb00ffeb077e7d95c18473fb9d5d565122
Rodrigo Santana <rodrigosantana.dev@gmail.com>
2025-06-12 18:40:17 -0300

    Finalizando criação do crud

--- src/models/FilmeService.js
+++ src/models/FilmeService.js
@@ -3,33 +3,40 @@ import { database } from '../configs/firebaseConfig';
 import Filme from './Filme';
 
 export default class FilmeService {
-  static async addFilme(filme) {
+  // CREATE
+  static async createFilme({ title, poster_path, genero, atores }) {
+    const filme = new Filme(null, title, poster_path, genero, atores);
     if (!filme.isValid()) {
       throw new Error('Dados do filme inválidos');
     }
+
     const filmesRef = ref(database, 'filmes');
     const newRef = push(filmesRef);
     await set(newRef, filme.toFirebase());
-    return newRef.key;
+    return Filme.fromFirebase(newRef.key, filme.toFirebase());
   }
 
-  static async updateFilme(filme) {
-    if (!filme.id) {
-      throw new Error('Filme precisa de ID para atualizar');
-    }
+  // READ
+  static async getAllFilmes(useCache = true) {
+    return await Filme.getFilmesFromFirebase(useCache);
+  }
+
+  // UPDATE
+  static async updateFilme(id, { title, poster_path, genero, atores }) {
+    const filme = new Filme(id, title, poster_path, genero, atores);
     if (!filme.isValid()) {
       throw new Error('Dados do filme inválidos');
     }
-    const filmeRef = ref(database, `filmes/${filme.id}`);
+
+    const filmeRef = ref(database, `filmes/${id}`);
     await set(filmeRef, filme.toFirebase());
+    return filme;
   }
 
+  // DELETE
   static async deleteFilme(id) {
+    if (!id) throw new Error('ID do filme é necessário para excluir');
     const filmeRef = ref(database, `filmes/${id}`);
     await remove(filmeRef);
-  }
-
-  static async getAllFilmes(useCache = true) {
-    return Filme.getFilmesFromFirebase(useCache);
   }
 }
