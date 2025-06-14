0525da2318585b038d2182848de63a4a3e80ce15
Luanv02 <limaluan32104@gmail.com>
2025-06-14 13:14:56 -0300

    add componente navi headerBar

---
+++ src/components/navi/HeaderBar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+const HeaderBar = ({ title, onBack }) => {
+  return (
+    <View style={styles.header}>
+      <TouchableOpacity onPress={onBack} style={styles.backButton}>
+        <Ionicons name="arrow-back" size={24} color="#FFF" />
+      </TouchableOpacity>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#192936',
+    height: 60,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 12,
+    justifyContent: 'center',
+    position: 'relative',
+  },
+  backButton: {
+    position: 'absolute',
+    left: 12,
+    zIndex: 1,
+  },
+  title: {
+    fontSize: 20,
+    color: '#FFF',
+    fontWeight: 'bold',
+  },
+});
+
+export default HeaderBar;
