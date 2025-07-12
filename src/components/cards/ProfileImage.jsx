a1dcb58d9080d0d613a8616ac8e0194898032beb
victorsouza00 <victorsouza183@gmail.com>
2025-07-12 13:12:31 -0300

    Atualização do menu , agora com o icone de usuario editavel.

--- src/components/cards/ProfileImage.jsx
+++ src/components/cards/ProfileImage.jsx
@@ -7,62 +7,67 @@ import { useContext , useState} from 'react';
 import { getAvatarUrl } from '../../services/getAvatarUrl';
 
 export default function ProfileImage({ 
-  size = 65, 
+  size = 70, 
   onPress, 
   style,
   children 
-}){
-    const { userCredentials, setUserCredentials } = useContext(UserContext);
-    const [source , setSource] =useState(null)
+}) {
+  const { userCredentials } = useContext(UserContext);
+  const [source, setSource] = useState(null);
 
-    useEffect(()=>{
-      getAvatarUrl(userCredentials.uid)
-      .then((url)=> setSource(url));
-    },[])
+  useEffect(() => {
+    getAvatarUrl(userCredentials.uid)
+      .then((url) => setSource(url));
+  }, []);
 
-
-    const containerStyle = {
+  const containerStyle = {
     width: size,
     height: size,
     borderRadius: size / 2,
-    };
+  };
 
-    {!source && (
-        <MaterialIcons name='person' size={size* 0.6} color={"#666"}></MaterialIcons>
-    )}
+  return (
+    <View style={[styles.wrapper, style]}>
+      <TouchableOpacity
+        onPress={async () => {
+          const newUrl = await pickAndUploadImage({ userid: userCredentials.uid });
+          setSource(newUrl);
+        }}
+        style={[styles.container, containerStyle]}
+      >
+        {!source && (
+          <MaterialIcons name='person' size={size * 0.6} color={"#666"} />
+        )}
 
-    return (
-  <TouchableOpacity 
-    onPress={
-      async () => {
-        const newUrl = await pickAndUploadImage({userid:userCredentials.uid}) 
-        setSource(newUrl)
-      }
-    }
-    style={[styles.container, containerStyle, style]}
-  >
-    {!source && (
-      <MaterialIcons name='person' size={size * 0.6} color={"#666"} />
-    )}
+        {source && (
+          <Image
+            source={{ uri: source }}
+            style={[styles.image, containerStyle]}
+            resizeMode="cover"
+          />
+        )}
 
-    {source && (
-      <Image
-        source={{uri : source}}
-        style={[styles.image, containerStyle]}
-        resizeMode="cover"
-      />
-    )}
+        {children}
+      </TouchableOpacity>
 
-    {children}
-  </TouchableOpacity>
-);
-
+      {/* Ícone de edição fora da imagem */}
+      <TouchableOpacity onPress={onPress} style={styles.editIconContainer}>
+        <MaterialIcons name="edit" size={16} color="white"/>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
+
 const styles = StyleSheet.create({
+  wrapper: {
+    position: 'relative',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   container: {
     overflow: 'hidden',
-    backgroundColor: '#e1e1e1', // cor de fundo caso a imagem não carregue
+    backgroundColor: '#334455',
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -70,4 +75,14 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
+  editIconContainer: {
+    position: 'absolute',
+    bottom: -5, // posição fora da imagem
+    right: -5,  // posição fora da imagem
+    backgroundColor: '#334455',
+    borderRadius: 12,
+    padding: 4,
+    zIndex: 10,
+  },
 });
+
